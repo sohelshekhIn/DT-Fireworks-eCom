29ef3bfc3f46d777b9729b01bf6a4afa7339262d
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-04-25 21:31:08 -0400

    Added Priline CSS Framework

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,16 +5,20 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#ff2525",
+        primaryDark: "#C62828",
+        secondary: "#FCF0CB",
+        secondaryDark: "#f7e19d",
+        accent: "#383838",
+        text: "#212121",
       },
     },
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
 export default config;
