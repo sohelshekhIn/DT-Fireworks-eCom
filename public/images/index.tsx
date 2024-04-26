fded31d8871e7fce531a4035764c39a9166b68c8
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-04-26 13:04:53 -0400

    Remove unused FireworksImage import in index.tsx

--- public/images/index.tsx
+++ public/images/index.tsx
@@ -1,4 +1,3 @@
-import FireworksImage from "./fireworks.jpg";
 import FireworksWithoutBg from "./firework-wtbg.png";
 
-export { FireworksImage, FireworksWithoutBg };
+export { FireworksWithoutBg };
