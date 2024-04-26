6668cc397710f72ab394fc14082bfb2c3d237bad
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-04-25 21:31:50 -0400

    Adding Navbar and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,250 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto border-t-2">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
+        <div className="col-span-full hidden lg:col-span-1 lg:block">
+          <Link
+            className="flex-none text-xl font-semibold text-primary hover:text-primaryDark dark:text-white"
+            href="#"
+            aria-label="Dt Fireworks"
+          >
+            DT Fireworks
+          </Link>
+          <p className="mt-3 text-xs sm:text-sm text-gray-600 dark:text-neutral-400">
+            © 2024 Dhanjibhai & Trikamlal Fireworks.
+          </p>
+        </div>
+
+        <div>
+          <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-neutral-100">
+            Shop
+          </h4>
+
+          <div className="mt-3 grid space-y-3 text-sm">
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/cart"
+              >
+                Cart
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/shop"
+              >
+                Shop all
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-neutral-100">
+            Company
+          </h4>
+
+          <div className="mt-3 grid space-y-3 text-sm">
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/about-us"
+              >
+                About us
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/testimonials"
+              >
+                Testimonials
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/sitemap"
+              >
+                Sitemap
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-neutral-100">
+            Resources
+          </h4>
+
+          <div className="mt-3 grid space-y-3 text-sm">
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Community
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Help & Support
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                eBook
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                What's New
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Status
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-neutral-100">
+            Developers
+          </h4>
+
+          <div className="mt-3 grid space-y-3 text-sm">
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Api
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Status
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                GitHub
+              </Link>{" "}
+              <span className="inline text-blue-600 dark:text-blue-500">
+                — New
+              </span>
+            </p>
+          </div>
+
+          <h4 className="mt-7 text-xs font-semibold text-gray-900 uppercase dark:text-neutral-100">
+            Industries
+          </h4>
+
+          <div className="mt-3 grid space-y-3 text-sm">
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Financial Services
+              </Link>
+            </p>
+            <p>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="#"
+              >
+                Education
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-5 mt-5 border-t border-gray-200 dark:border-neutral-700">
+        <div className="sm:flex sm:justify-between sm:items-center">
+          <div className="flex items-center gap-x-3">
+            <div className="space-x-4 text-sm ms-4">
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/terms"
+              >
+                Terms
+              </Link>
+              <Link
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="/privacy"
+              >
+                Privacy
+              </Link>
+              <Link
+                target="_blank"
+                className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-neutral-400 dark:hover:text-neutral-200"
+                href="https://status.dtfireworks.in"
+              >
+                Status
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center mt-5">
+            <div className="mt-3 sm:hidden">
+              <Link
+                className="flex-none text-xl font-semibold dark:text-white"
+                href="#"
+                aria-label="Brand"
+              >
+                DT Fireworks
+              </Link>
+              <p className="mt-1 text-xs sm:text-sm text-gray-600 dark:text-neutral-400">
+                © 2024 Dhanjibhai & Trikamlal Fireworks.
+              </p>
+            </div>
+
+            <div className="text-xs text-right">
+              <Link
+                className="inline-block underline text-gray-500 hover:text-gray-800 dark:text-neutral-500 dark:hover:text-neutral-200"
+                href="https://sohel.tech?ref=dtfireworks.in"
+              >
+                Designed & Developed by Sohel Shekh
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,166 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7">
+      <nav
+        className="relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 basis-full items-center px-4 md:px-8 mx-auto"
+        aria-label="Global"
+      >
+        <div className="md:col-span-3">
+          <Link
+            className="flex-none rounded-xl text-primary hover:text-primaryDark text-xl inline-block font-semibold focus:outline-none focus:opacity-80"
+            href="/"
+            aria-label="DT Fireworks"
+          >
+            DT Fireworks
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
+          <button
+            type="button"
+            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white"
+          >
+            Cart
+          </button>
+          <button
+            type="button"
+            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-xl border border-transparent bg-primary text-secondary hover:bg-primaryDark transition disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-lime-500"
+          >
+            Sign In
+          </button>
+
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle size-[38px] flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700"
+              data-hs-collapse="#navbar-collapse-with-animation"
+              aria-controls="navbar-collapse-with-animation"
+              aria-label="Toggle navigation"
+            >
+              <svg
+                className="hs-collapse-open:hidden flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block hidden flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div
+          id="navbar-collapse-with-animation"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6"
+        >
+          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
+            <div>
+              <Link
+                className="inline-block text-black hover:under-design dark:text-white"
+                href="/"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </div>
+            <div>
+              <Link
+                className="inline-block text-black hover:text-gray-600 hover:under-design dark:text-white dark:hover:text-neutral-300"
+                href="/shop"
+              >
+                Shop
+              </Link>
+            </div>
+            <div className="hs-dropdown [--strategy:static] group sm:[--strategy:fixed] [--adaptive:none] sm:[--trigger:hover] sm:py-4">
+              <button
+                type="button"
+                className="flex items-center w-full text-black group-hover:under-design hover:text-black font-normal   dark:text-white dark:hover:text-white"
+              >
+                Categories
+                {/* <svg
+                  className="flex-shrink-0 ms-2 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m6 9 6 6 6-6" />
+                </svg> */}
+              </button>
+
+              <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-neutral-800 sm:dark:border dark:border-neutral-700 dark:divide-neutral-700 before:absolute top-full sm:border before:-top-5 before:start-0 before:w-full before:h-5">
+                <Link
+                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-secondaryDark dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300"
+                  href="#"
+                >
+                  About
+                </Link>
+
+                <Link
+                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-secondaryDark dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300"
+                  href="#"
+                >
+                  Downloads
+                </Link>
+                <Link
+                  className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-secondaryDark dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300"
+                  href="# "
+                >
+                  Team Account
+                </Link>
+              </div>
+            </div>
+            <div>
+              <Link
+                className="inline-block text-black hover:under-design hover:text-gray-600 dark:text-white dark:hover:text-neutral-300"
+                href="/about-us"
+              >
+                About Us
+              </Link>
+            </div>
+            <div>
+              <Link
+                className="inline-block text-black hover:text-gray-600 hover:under-design  dark:text-white dark:hover:text-neutral-300"
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
