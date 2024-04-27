ae2439a1fa35e32a3dc471cbf50084cd62de1847
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-04-27 02:48:45 -0400

    Update Navbar component to remove commented out SVG code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -104,20 +104,6 @@ const Navbar = () => {
                 className="flex items-center w-full text-black group-hover:under-design hover:text-black font-normal   dark:text-white dark:hover:text-white"
               >
                 Categories
-                {/* <svg
-                  className="flex-shrink-0 ms-2 size-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="m6 9 6 6 6-6" />
-                </svg> */}
               </button>
 
               <div className="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-neutral-800 sm:dark:border dark:border-neutral-700 dark:divide-neutral-700 before:absolute top-full sm:border before:-top-5 before:start-0 before:w-full before:h-5">
