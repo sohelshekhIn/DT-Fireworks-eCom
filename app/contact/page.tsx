bbe38a7b67f377076474cca2a90723645363e33d
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-04-28 17:36:12 -0400

    Update ContactPage component with responsive map iframe in page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 const ContactPage = () => {
   return (
     <div className="max-w-7xl px-4 lg:px-8 py-12 lg:py-24 mx-auto">
@@ -12,12 +10,8 @@ const ContactPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 lg:items-center gap-6 md:gap-8 lg:gap-12">
         <div className="aspect-w-16 aspect-h-6 lg:aspect-h-16 overflow-hidden bg-gray-100 rounded-2xl dark:bg-neutral-800">
           <iframe
+            className="w-full h-[50vh] lg:h-[70vh] border-0"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.852317207524!2d72.85816871170235!3d22.696540979317678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e5b0e7ca91cb7%3A0x4ca530eec709c930!2sDHANJIBHAI%20%26%20TRIKAMLAL!5e0!3m2!1sen!2sca!4v1714089000087!5m2!1sen!2sca"
-            width="600"
-            height="450"
-            style={{
-              border: 0,
-            }}
             allowFullScreen={false}
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
