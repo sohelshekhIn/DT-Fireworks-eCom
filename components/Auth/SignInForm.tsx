aa517527e047803a98a30692d7ecfbd0fcd66ccd
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-05-12 03:25:20 -0400

    feat: Improve mobile responsiveness of sign-in and sign-up pages

--- components/Auth/SignInForm.tsx
+++ components/Auth/SignInForm.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { auth, provider } from "@/lib/firebase-config";
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth } from "@/lib/firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const SignInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
   const handleSignIn = (
@@ -16,8 +17,30 @@ const SignInForm = () => {
       | React.FormEvent<HTMLFormElement>
   ) => {
     e.preventDefault();
-    console.time("SignIn");
-    console.time("CallAPI");
+
+    // check if email and password are empty
+    if (!email || !password) {
+      setError("Email and password are required");
+      return;
+    }
+
+    // check if email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    // check if password is at least 8 characters and contains a number
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      setError("Password must be at least 8 characters and contain a number");
+      return;
+    }
+    setError(null);
+
+    // console.time("SignIn");
+    // console.time("CallAPI");
     signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
       const user = userCredential.user;
       const setCookie = new Promise(async () => {
@@ -25,17 +48,16 @@ const SignInForm = () => {
           method: "POST",
           headers: {
             Authorization: `Bearer ${await user.getIdToken()}`,
+            credentials: "same-origin",
           },
-        }).then(() => console.timeEnd("CallAPI"));
+        });
       });
       const redirect = new Promise(() => {
-        console.timeEnd("SignIn");
+        // console.timeEnd("SignIn");
         router.push("/protected/client");
       });
 
-      Promise.all([setCookie, redirect]).then((values) => {
-        console.log(values);
-      });
+      Promise.all([setCookie, redirect]);
     });
   };
 
@@ -84,35 +106,70 @@ const SignInForm = () => {
               Password
             </label>
             <a
-              className="text-sm text-blue-600 decoration-2 hover:underline font-medium"
-              href="../examples/html/recover-account.html"
+              className="text-sm text-primaryDark decoration-2 hover:underline font-medium"
+              href="/forgot-password"
             >
               Forgot password?
             </a>
           </div>
           <div className="relative">
             <input
+              id="hs-toggle-password"
               type="password"
-              id="password"
+              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              placeholder="Enter password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              name="password"
-              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              required
-              aria-describedby="password-error"
             />
-            <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+            <button
+              type="button"
+              data-hs-toggle-password='{
+        "target": "#hs-toggle-password"
+      }'
+              className="absolute top-0 end-0 p-3.5 rounded-e-md"
+            >
               <svg
-                className="size-5 text-red-500"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                aria-hidden="true"
+                className="flex-shrink-0 size-3.5 text-gray-400 dark:text-neutral-600"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                <path
+                  className="hs-password-active:hidden"
+                  d="M9.88 9.88a3 3 0 1 0 4.24 4.24"
+                ></path>
+                <path
+                  className="hs-password-active:hidden"
+                  d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
+                ></path>
+                <path
+                  className="hs-password-active:hidden"
+                  d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
+                ></path>
+                <line
+                  className="hs-password-active:hidden"
+                  x1="2"
+                  x2="22"
+                  y1="2"
+                  y2="22"
+                ></line>
+                <path
+                  className="hidden hs-password-active:block"
+                  d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"
+                ></path>
+                <circle
+                  className="hidden hs-password-active:block"
+                  cx="12"
+                  cy="12"
+                  r="3"
+                ></circle>
               </svg>
-            </div>
+            </button>
           </div>
           <p className="hidden text-xs text-red-600 mt-2" id="password-error">
             8+ characters required
@@ -125,7 +182,7 @@ const SignInForm = () => {
               id="remember-me"
               name="remember-me"
               type="checkbox"
-              className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+              className="shrink-0 mt-0.5 border-gray-200 rounded text-primary focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-primary dark:checked:border-primary dark:focus:ring-offset-gray-800"
             />
           </div>
           <div className="ms-3">
@@ -134,11 +191,16 @@ const SignInForm = () => {
             </label>
           </div>
         </div>
+        {error && (
+          <p className="text-red-500 dark:text-red-600/80 py-2 underline font-medium">
+            {error}
+          </p>
+        )}
 
         <button
           type="submit"
           onClick={handleSignIn}
-          className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+          className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary text-white hover:bg-primaryDark disabled:opacity-50 disabled:pointer-events-none"
         >
           Sign in
         </button>
--- components/Auth/SignUpForm.tsx
+++ components/Auth/SignUpForm.tsx
@@ -2,12 +2,78 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase-config";
 
 const SignUpForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
+
+  const handleSignUp = (
+    e:
+      | React.MouseEvent<HTMLButtonElement, MouseEvent>
+      | React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    // check if email and password are empty
+    if (!email || !password || !confirmPassword) {
+      setError("Email and password are required");
+      return;
+    }
+
+    // check if email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    // check if password is at least 8 characters and contains a number
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+      setError("Password must be at least 8 characters and contain a number");
+      return;
+    }
+
+    // check if password and confirm password match
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    setError(null);
+
+    // TODO:
+    // Check password regex
+    // Make new route for after sign up
+
+    createUserWithEmailAndPassword(auth, email, password).then(
+      (userCredential) => {
+        const user = userCredential.user;
+        const setCookie = new Promise(async () => {
+          fetch("/api/login", {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${await user.getIdToken()}`,
+              credentials: "same-origin",
+            },
+          }).catch((error) => {
+            console.error("Error:", error);
+            setError("An error occurred. Please try again.");
+          });
+        });
+        const redirect = new Promise(() => {
+          router.push("/protected/client");
+        });
+
+        Promise.all([setCookie, redirect]);
+      }
+    );
+  };
 
   return (
     <form>
@@ -20,127 +86,163 @@ const SignUpForm = () => {
             <input
               type="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               name="email"
-              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primaryDark focus:ring-primaryDark disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
               required
               aria-describedby="email-error"
             />
-            <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-              <svg
-                className="size-5 text-red-500"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                aria-hidden="true"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-              </svg>
-            </div>
           </div>
-          <p className="hidden text-xs text-red-600 mt-2" id="email-error">
-            Please include a valid email address so we can get back to you
-          </p>
         </div>
 
-        <div>
-          <label
-            htmlFor="password"
-            className="block text-sm mb-2 dark:text-white"
-          >
-            Password
-          </label>
-          <div className="relative">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              required
-              aria-describedby="password-error"
-            />
-            <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-              <svg
-                className="size-5 text-red-500"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                aria-hidden="true"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-              </svg>
-            </div>
-          </div>
-          <p className="hidden text-xs text-red-600 mt-2" id="password-error">
-            8+ characters required
-          </p>
-        </div>
-
-        <div>
-          <label
-            htmlFor="confirm-password"
-            className="block text-sm mb-2 dark:text-white"
-          >
-            Confirm Password
-          </label>
-          <div className="relative">
-            <input
-              type="password"
-              id="confirm-password"
-              name="confirm-password"
-              className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-              required
-              aria-describedby="confirm-password-error"
-            />
-            <div className="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-              <svg
-                className="size-5 text-red-500"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                aria-hidden="true"
-              >
-                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-              </svg>
-            </div>
-          </div>
-          <p
-            className="hidden text-xs text-red-600 mt-2"
-            id="confirm-password-error"
-          >
-            Password does not match the password
-          </p>
-        </div>
-        <div className="flex items-center">
-          <div className="flex">
-            <input
-              id="remember-me"
-              name="remember-me"
-              type="checkbox"
-              className="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-            />
-          </div>
-          <div className="ms-3">
-            <label htmlFor="remember-me" className="text-sm dark:text-white">
-              I accept the{" "}
-              <a
-                className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
-                href="#"
-              >
-                Terms and Conditions
-              </a>
+        <div className="space-y-5" data-hs-toggle-password-group="">
+          <div className="max-w-sm">
+            <label
+              htmlFor="hs-toggle-password-multi-toggle-np"
+              className="block text-sm mb-2 dark:text-white"
+            >
+              Enter password
             </label>
+            <div className="relative">
+              <input
+                id="hs-toggle-password-multi-toggle-np"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primaryDark focus:ring-primaryDark disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="Enter password"
+              />
+              <button
+                type="button"
+                data-hs-toggle-password='{
+          "target": ["#hs-toggle-password-multi-toggle", "#hs-toggle-password-multi-toggle-np"]
+        }'
+                className="absolute top-0 end-0 p-3.5 rounded-e-md"
+              >
+                <svg
+                  className="flex-shrink-0 size-3.5 text-gray-400 dark:text-neutral-600"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M9.88 9.88a3 3 0 1 0 4.24 4.24"
+                  ></path>
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
+                  ></path>
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
+                  ></path>
+                  <line
+                    className="hs-password-active:hidden"
+                    x1="2"
+                    x2="22"
+                    y1="2"
+                    y2="22"
+                  ></line>
+                  <path
+                    className="hidden hs-password-active:block"
+                    d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"
+                  ></path>
+                  <circle
+                    className="hidden hs-password-active:block"
+                    cx="12"
+                    cy="12"
+                    r="3"
+                  ></circle>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="max-w-sm mb-5">
+            <label
+              htmlFor="hs-toggle-password-multi-toggle"
+              className="block text-sm mb-2 dark:text-white"
+            >
+              Confirm password
+            </label>
+            <div className="relative">
+              <input
+                id="hs-toggle-password-multi-toggle"
+                type="password"
+                className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primaryDark focus:ring-primaryDark disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                data-hs-toggle-password='{
+          "target": ["#hs-toggle-password-multi-toggle", "#hs-toggle-password-multi-toggle-np"]
+        }'
+                className="absolute top-0 end-0 p-3.5 rounded-e-md"
+              >
+                <svg
+                  className="flex-shrink-0 size-3.5 text-gray-400 dark:text-neutral-600"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M9.88 9.88a3 3 0 1 0 4.24 4.24"
+                  ></path>
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
+                  ></path>
+                  <path
+                    className="hs-password-active:hidden"
+                    d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
+                  ></path>
+                  <line
+                    className="hs-password-active:hidden"
+                    x1="2"
+                    x2="22"
+                    y1="2"
+                    y2="22"
+                  ></line>
+                  <path
+                    className="hidden hs-password-active:block"
+                    d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"
+                  ></path>
+                  <circle
+                    className="hidden hs-password-active:block"
+                    cx="12"
+                    cy="12"
+                    r="3"
+                  ></circle>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
-
-        <button
-          type="submit"
-          className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-        >
-          Sign up
-        </button>
+        {error && <p className="text-red-600 text-sm mt-2">{error}</p>}
+        <div className="mt-5">
+          <button
+            onClick={handleSignUp}
+            type="submit"
+            className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+          >
+            Sign up
+          </button>
+        </div>
       </div>
     </form>
   );
