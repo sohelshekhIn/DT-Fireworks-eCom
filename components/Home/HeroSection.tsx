eba7b6f45132fcbabafe79240904c19e42b50c61
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-05-12 02:01:08 -0400

    Update HeroSection heading font size for better readability

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
     <section className="max-w-[85rem] h-[70vh] lg:h-[60vh] pt-16 mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col-reverse lg:flex-row lg:items-center">
         <div className="mt-10 lg:mt-0">
-          <h1 className=" font-bold flex flex-col text-base sm:text-lg md:text-xl lg:text-2xl text-gray-800 dark:text-white">
+          <h1 className=" font-bold flex flex-col text-base sm:text-lg md:text-xl lg:text-3xl text-gray-800 dark:text-white">
             Experience the
-            <span className=" text-3xl sm:text-4xl md:text-5xl lg:text-6xl hover:text-primary ">
+            <span className=" text-3xl sm:text-4xl md:text-5xl lg:text-7xl hover:text-primary ">
               Magic of Fireworks
             </span>
           </h1>
