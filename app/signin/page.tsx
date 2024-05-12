d563e5d96dd30a9c3970e9afca90940cd9c30287
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-05-12 03:25:14 -0400

    Update sign-in and sign-up pages' layout for better mobile responsiveness

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <div className="max-w-[85vw] mx-auto py-40">
+    <div className="max-w-[85vw] mx-auto py-5 md:py-10 lg:py-40">
       <div className="min-w-80 max-w-3xl p-4 mx-auto bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
         <div className="p-4 sm:p-7">
           <div className="text-center">
@@ -14,7 +14,7 @@ const SignInPage = () => {
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
               Don't have an account yet?{" "}
               <Link
-                className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
+                className="text-primary decoration-2 hover:underline font-medium dark:text-primary"
                 href="/signup"
               >
                 Sign up here
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const SignUpPage = () => {
   return (
-    <div className="max-w-[85vw] mx-auto py-40">
+    <div className="max-w-[85vw] mx-auto py-10 md:py-16 lg:py-40">
       <div className="mt-7 p-4 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
         <div className="p-4 sm:p-7">
           <div className="text-center">
@@ -14,7 +14,7 @@ const SignUpPage = () => {
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
               Already have an account?
               <Link
-                className="text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500"
+                className="text-primary decoration-2 hover:underline font-medium dark:text-primaryDark"
                 href="/signin"
               >
                 Sign in here
