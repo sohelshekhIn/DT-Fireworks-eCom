6ed944bcbf2025749a0386a37bfe2a81d691c27b
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 15:03:48 -0400

    refactor: Update CategoryPage component to fetch and display products in a specific category

--- app/shop/category/[categoryId]/page.tsx
+++ app/shop/category/[categoryId]/page.tsx
@@ -1,8 +1,12 @@
-import { NotFoundSapien } from "@/public/images";
+import {
+  CategoryNotFound,
+  NoProductsFound,
+} from "@/components/Shop/ErrorComps";
+import ProductCard from "@/components/Shop/ProductCard";
 import { Category } from "@/types/category";
+import { Product } from "@/types/product";
 import appUrl from "@/utils/apiCallHandler";
 import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const CategoryPage = async ({
@@ -12,6 +16,10 @@ const CategoryPage = async ({
     categoryId: string;
   };
 }) => {
+  if (params.categoryId == "") {
+    return redirect("/");
+  }
+
   const getCategory = async (categoryId: string) => {
     const res = await fetch(
       appUrl("/api/categories/one?category=" + categoryId),
@@ -25,26 +33,62 @@ const CategoryPage = async ({
     return data;
   };
 
-  if (params.categoryId == "") {
-    return redirect("/");
-  }
+  const getProducts = async (categoryId: string) => {
+    const res = await fetch(
+      appUrl("/api/products/all?category=" + categoryId),
+      {
+        next: {
+          tags: ["occassion-products"],
+          revalidate:
+            60 * parseInt(process.env.NEXT_PUBLIC_API_REVALIDATE || "60"),
+        },
+      }
+    );
+    const status = res.status;
+    if (status === 404) {
+      return { data: null };
+    }
+    const data = await res.json();
+    return data;
+  };
 
   var category: Category | null = null;
+  var products: Product[] | null = null;
   const data = await getCategory(params.categoryId);
   if (!data.data) {
     return <CategoryNotFound />;
   } else {
     category = data.data;
+    const productsData = await getProducts(params.categoryId);
+
+    if (!productsData.data || productsData.data.length === 0) {
+      return (
+        category && (
+          <>
+            <CategoryHeader category={category} />
+            <NoProductsFound />
+          </>
+        )
+      );
+    } else {
+      products = productsData.data;
+    }
   }
 
   return (
     category && (
       <section
         className="
-        w-[85vw] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
+        w-[85vw] px-4 pb-10 sm:px-6 lg:px-8 lg:pb-14 mx-auto
     "
       >
-        {category.name}
+        <CategoryHeader category={category} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 lg:mt-14">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
       </section>
     )
   );
@@ -52,34 +96,31 @@ const CategoryPage = async ({
 
 export default CategoryPage;
 
-const CategoryNotFound = () => {
+const CategoryHeader = ({ category }: { category: Category }) => {
   return (
-    <section
-      className="
-        w-[85vw] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
-    "
-    >
-      <div className="max-w-2xl min-h-dvh mx-auto text-center mb-10 lg:mb-14">
+    <div className="w-full rounded-t-xl overflow-hidden">
+      <div className="relative">
         <Image
-          width={500}
-          height={500}
-          className="mx-auto"
-          src={NotFoundSapien}
-          alt="A male standing in nowhere describing the Category user was looking is not found & does not exists"
+          width={1200}
+          height={300}
+          className="w-full max-h-96 object-cover object-center"
+          src={category.thumb_image}
+          alt="Header Image for occassion"
         />
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-          Category/Occassion Not Found
-        </h2>
-        <p className="mt-1 text-gray-600 dark:text-neutral-400">
-          The category/occassion you are looking for is not available.
-        </p>
-        <Link
-          href="/shop"
-          className="mt-10 text-primary dark:text-primaryDark font-semibold underline"
-        >
-          Go Back to Shop
-        </Link>
+        <div
+          className="absolute z-[2] inset-0 h-full w-full
+             bg-gradient-to-t from-black via-black/80 to-transparent"
+        ></div>
+        <div className="absolute w-full h-full z-[3] top-20 p-5 text-secondaryDark flex flex-col justify-center items-center">
+          <h2 className="text-2xl w-full text-center font-bold md:text-4xl md:leading-tight dark:text-white">
+            {category.name}
+          </h2>
+          <p className="mt-1 text-white/70 text-center w-full">
+            Decorate your wedding stage with our Wedding Fireworks & SFX
+            package.
+          </p>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
