cd4a10b4681ad2ca068c11a6358b1df8150a3c3b
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 02:19:38 -0400

    feat: Add NotFoundSapien image to image exports

--- public/images/index.tsx
+++ public/images/index.tsx
@@ -1,4 +1,5 @@
 import FireworksWithoutBg from "./firework-wtbg.png";
 import HeroImage from "./hero_slider_1.png";
+import NotFoundSapien from "./not-found.svg";
 
-export { FireworksWithoutBg, HeroImage };
+export { FireworksWithoutBg, HeroImage, NotFoundSapien };
