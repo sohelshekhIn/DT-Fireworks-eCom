6397c0d05e794f82a3d5bed5c1026adc5c06d2ef
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 02:28:51 -0400

    refactor: Add API routes for fetching all categories and a specific category

---
+++ app/api/occassions/all/route.ts
@@ -0,0 +1,47 @@
+import { customInitApp } from "@/lib/firebase-admin-config";
+import { db } from "@/lib/firebase-config";
+import { CustomError, handleApiError } from "@/utils/apiErrorHandler";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { NextResponse } from "next/server";
+
+type Category = {
+  id: string;
+  name: string;
+  thumb_image: string;
+};
+
+// Init the Firebase SDK every time the server is called
+customInitApp();
+
+export async function GET() {
+  try {
+    let categories: Category[] = [];
+    const categoryQuery = query(
+      collection(db, "categories"),
+      where("isVisible", "==", true),
+      where("isOccassion", "==", true)
+    );
+
+    await getDocs(categoryQuery)
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          categories.push({
+            id: doc.id,
+            name: doc.data().name,
+            thumb_image: doc.data().thumb_image,
+          });
+        });
+      })
+      .catch((error) => {
+        throw new CustomError(error.toString(), error.code);
+      });
+
+    return NextResponse.json({ data: categories }, { status: 200 });
+  } catch (error: any) {
+    return handleApiError(error);
+  }
+}
+
+export const revalidate =
+  60 * parseInt(process.env.NEXT_PUBLIC_FIRESTORE_REVALIDATE || "60");
+// This is the revalidate time in minutes. Fallsback to 60 minutes if not provided in .env
---
+++ app/api/occassions/one/route.ts
@@ -0,0 +1,47 @@
+import { customInitApp } from "@/lib/firebase-admin-config";
+import { db } from "@/lib/firebase-config";
+import { Occassion } from "@/types/category";
+import { CustomError, handleApiError } from "@/utils/apiErrorHandler";
+import { getDoc, doc } from "firebase/firestore";
+import { NextRequest, NextResponse } from "next/server";
+
+customInitApp();
+
+export async function GET(req: NextRequest) {
+  try {
+    const url = new URL(req.nextUrl);
+    const occassionId = url.searchParams.get("occassion");
+    var occassion: Object = {};
+    var responseCode = 500;
+    if (occassionId) {
+      const occassionRef = doc(db, "categories", occassionId);
+
+      await getDoc(occassionRef)
+        .then((doc) => {
+          if (doc.exists()) {
+            occassion = doc.data() as Occassion;
+            responseCode = 200;
+          } else {
+            throw new CustomError("Occassion not found.", 404);
+          }
+        })
+        .catch((error) => {
+          throw new CustomError(error.message, error.code);
+        });
+    } else {
+      throw new CustomError("Invalid request: OccassionId not found.", 400);
+    }
+    return NextResponse.json(
+      {
+        data: occassion,
+      },
+      { status: responseCode }
+    );
+  } catch (error: any) {
+    return handleApiError(error);
+  }
+}
+
+export const revalidate =
+  60 * parseInt(process.env.NEXT_PUBLIC_FIRESTORE_REVALIDATE || "60");
+// This is the revalidate time in minutes. Fallsback to 60 minutes if not provided in .env
