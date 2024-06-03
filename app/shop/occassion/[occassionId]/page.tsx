498a71e8026c393df0377d642a309e8741afab6c
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 15:04:12 -0400

    refactor: Update ErrorComps components to improve error messages and add new component for displaying no products found

--- app/shop/occassion/[occassionId]/page.tsx
+++ app/shop/occassion/[occassionId]/page.tsx
@@ -1,5 +1,10 @@
-import { OccassionNotFound } from "@/components/Shop/ErrorComps";
+import {
+  NoProductsFound,
+  OccassionNotFound,
+} from "@/components/Shop/ErrorComps";
+import ProductCard from "@/components/Shop/ProductCard";
 import { Occassion } from "@/types/category";
+import { Product } from "@/types/product";
 import appUrl from "@/utils/apiCallHandler";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -11,6 +16,10 @@ const OccasionPage = async ({
     occassionId: string;
   };
 }) => {
+  if (params.occassionId == "") {
+    return redirect("/");
+  }
+
   const getOccassion = async (occassionId: string) => {
     const res = await fetch(
       appUrl("/api/occassions/one?occassion=" + occassionId),
@@ -22,57 +31,103 @@ const OccasionPage = async ({
         },
       }
     );
+    const status = res.status;
+    if (status === 404) {
+      return { data: null };
+    }
+    const data = await res.json();
+    return data;
+  };
+  const getProducts = async (occassionId: string) => {
+    const res = await fetch(
+      appUrl("/api/products/all?category=" + occassionId),
+      {
+        next: {
+          tags: ["occassion-products"],
+          revalidate:
+            60 * parseInt(process.env.NEXT_PUBLIC_API_REVALIDATE || "60"),
+        },
+      }
+    );
+    const status = res.status;
+    if (status === 404) {
+      return { data: null };
+    }
     const data = await res.json();
     return data;
   };
 
-  if (params.occassionId == "") {
-    return redirect("/");
-  }
-
   var occassion: Occassion | null = null;
-  const data = await getOccassion(params.occassionId);
-  if (!data.data) {
+  var products: Product[] | null = null;
+
+  const occassionData = await getOccassion(params.occassionId);
+  if (!occassionData.data) {
     return <OccassionNotFound />;
   } else {
-    occassion = data.data;
+    occassion = occassionData.data;
+    const productsData = await getProducts(params.occassionId);
+
+    if (!productsData.data || productsData.data.length === 0) {
+      return (
+        occassion && (
+          <>
+            <OccassionHeader occassion={occassion} />
+            <NoProductsFound />
+          </>
+        )
+      );
+    } else {
+      products = productsData.data;
+    }
   }
 
-  // fetch products from this occassion
-
   return (
-    occassion && (
+    occassion &&
+    products && (
       <section
         className="w-[85vw] px-4 pb-10 sm:px-6 lg:px-8 lg:pb-14 mx-auto
     "
       >
-        <div className="w-full">
-          <div className="relative">
-            <Image
-              width={1200}
-              height={300}
-              className="w-full max-h-96 object-cover object-center"
-              src={occassion.thumb_image}
-              alt="Header Image for occassion"
-            />
-            <div
-              className="absolute z-[2] inset-0 h-full w-full
-             bg-gradient-to-t from-black via-black/80 to-transparent"
-            ></div>
-            <div className="absolute w-full h-full z-[3] top-20 p-5 text-secondaryDark flex flex-col justify-center items-center">
-              <h2 className="text-2xl w-full text-center font-bold md:text-4xl md:leading-tight dark:text-white">
-                {occassion.name}
-              </h2>
-              <p className="mt-1 text-white/70 text-center w-full">
-                Decorate your wedding stage with our Wedding Fireworks & SFX
-                package.
-              </p>
-            </div>
+        <OccassionHeader occassion={occassion} />
+        {
+          <div className="">
+            {products.map((product: Product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
           </div>
-        </div>
+        }
       </section>
     )
   );
 };
 
 export default OccasionPage;
+
+const OccassionHeader = ({ occassion }: { occassion: Occassion }) => {
+  return (
+    <div className="w-full rounded-t-xl overflow-hidden">
+      <div className="relative">
+        <Image
+          width={1200}
+          height={300}
+          className="w-full max-h-96 object-cover object-center"
+          src={occassion.thumb_image}
+          alt="Header Image for occassion"
+        />
+        <div
+          className="absolute z-[2] inset-0 h-full w-full
+             bg-gradient-to-t from-black via-black/80 to-transparent"
+        ></div>
+        <div className="absolute w-full h-full z-[3] top-20 p-5 text-secondaryDark flex flex-col justify-center items-center">
+          <h2 className="text-2xl w-full text-center font-bold md:text-4xl md:leading-tight dark:text-white">
+            {occassion.name}
+          </h2>
+          <p className="mt-1 text-white/70 text-center w-full">
+            Decorate your wedding stage with our Wedding Fireworks & SFX
+            package.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- components/Shop/ErrorComps.tsx
+++ components/Shop/ErrorComps.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const OccassionNotFound = () => {
   return (
-    <section
+    <div
       className="
-        w-[85vw] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
+        w-full px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
     "
     >
       <div className="max-w-2xl min-h-dvh mx-auto text-center mb-10 lg:mb-14">
@@ -30,13 +30,44 @@ const OccassionNotFound = () => {
           Go Back to Shop
         </Link>
       </div>
+    </div>
+  );
+};
+const CategoryNotFound = () => {
+  return (
+    <section
+      className="
+        w-[85vw] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
+    "
+    >
+      <div className="max-w-2xl min-h-dvh mx-auto text-center mb-10 lg:mb-14">
+        <Image
+          width={500}
+          height={500}
+          className="mx-auto"
+          src={NotFoundSapien}
+          alt="A male standing in nowhere describing the Category user was looking is not found & does not exists"
+        />
+        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+          Category/Occassion Not Found
+        </h2>
+        <p className="mt-1 text-gray-600 dark:text-neutral-400">
+          The category/occassion you are looking for is not available.
+        </p>
+        <Link
+          href="/shop"
+          className="mt-10 text-primary dark:text-primaryDark font-semibold underline"
+        >
+          Go Back to Shop
+        </Link>
+      </div>
     </section>
   );
 };
 
 const OccassionFetchError = () => {
   return (
-    <section className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <div className="w-full px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <div className="max-w-2xl text-center mx-auto mb-10 lg:mb-14">
         <Image
           width={500}
@@ -53,8 +84,43 @@ const OccassionFetchError = () => {
           later.
         </p>
       </div>
-    </section>
+    </div>
   );
 };
 
-export { OccassionNotFound, OccassionFetchError };
+const NoProductsFound = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className="flex p-20 items-center">
+        <Image
+          width={300}
+          height={300}
+          className="mx-auto p-5"
+          src={NotFoundSapien}
+          alt="No Products Found"
+        />
+        <div className="">
+          <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+            Category is currently empty!
+          </h2>
+          <p className="mt-1 text-gray-600 dark:text-neutral-400">
+            No products found in the category you are looking...
+          </p>
+          <Link
+            href="/shop"
+            className="mt-10 text-primary dark:text-primaryDark font-semibold underline"
+          >
+            Go Back to Shop
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export {
+  OccassionNotFound,
+  OccassionFetchError,
+  NoProductsFound,
+  CategoryNotFound,
+};
