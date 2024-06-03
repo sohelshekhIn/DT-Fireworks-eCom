1bd88d42928de2b7a7c4ec0fb2cf20817b18bb31
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 13:34:13 -0400

    refactor: Update Product interface to use string for id and add product_id field

--- types/product.ts
+++ types/product.ts
@@ -1,13 +1,18 @@
 export interface Product {
-  id: number;
+  id: string;
+  product_id: number;
   name: string;
-  slug: string;
-  description: string;
+  // slug: string;
+  // description: string;
   price: number;
   images: string[];
   categories: string[];
-  rating: number;
-  stock: number;
-  onSale: boolean;
-  discount: number;
+  // rating: number;
+  // stock: number;
+  // onSale: boolean;
+  discount: {
+    id: string;
+    name: string;
+    value: number; // percentage
+  };
 }
