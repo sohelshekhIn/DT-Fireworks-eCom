83af8222754e494eedd8adb99304f75d35c041b0
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 03:56:51 -0400

    refactor: Update OccasionPage component to improve header image display and add description

--- app/shop/occassion/[occassionId]/page.tsx
+++ app/shop/occassion/[occassionId]/page.tsx
@@ -38,25 +38,38 @@ const OccasionPage = async ({
     occassion = data.data;
   }
 
+  // fetch products from this occassion
+
   return (
     occassion && (
       <section
-        className="
-        w-[85vw] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto
+        className="w-[85vw] px-4 pb-10 sm:px-6 lg:px-8 lg:pb-14 mx-auto
     "
       >
-        <div className="h-32 w-full overflow-hidden">
-          <Image
-            width={600}
-            height={200}
-            src={occassion.thumb_image}
-            alt="Header Image for occassion"
-          />
+        <div className="w-full">
+          <div className="relative">
+            <Image
+              width={1200}
+              height={300}
+              className="w-full max-h-96 object-cover object-center"
+              src={occassion.thumb_image}
+              alt="Header Image for occassion"
+            />
+            <div
+              className="absolute z-[2] inset-0 h-full w-full
+             bg-gradient-to-t from-black via-black/80 to-transparent"
+            ></div>
+            <div className="absolute w-full h-full z-[3] top-20 p-5 text-secondaryDark flex flex-col justify-center items-center">
+              <h2 className="text-2xl w-full text-center font-bold md:text-4xl md:leading-tight dark:text-white">
+                {occassion.name}
+              </h2>
+              <p className="mt-1 text-white/70 text-center w-full">
+                Decorate your wedding stage with our Wedding Fireworks & SFX
+                package.
+              </p>
+            </div>
+          </div>
         </div>
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
-          {occassion.name}
-        </h2>
-        <p className="mt-1 text-gray-600 dark:text-neutral-400"></p>
       </section>
     )
   );
