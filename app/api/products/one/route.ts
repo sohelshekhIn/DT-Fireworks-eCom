b9a675c5dc85a4578ab4f5062727ed9a402302f9
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-03 20:10:04 -0400

    refactor: Add API route for fetching a single product by ID

---
+++ app/api/products/one/route.ts
@@ -0,0 +1,47 @@
+import { customInitApp } from "@/lib/firebase-admin-config";
+import { db } from "@/lib/firebase-config";
+import { Product } from "@/types/product";
+import { CustomError, handleApiError } from "@/utils/apiErrorHandler";
+import { getDoc, doc } from "firebase/firestore";
+import { NextRequest, NextResponse } from "next/server";
+
+customInitApp();
+
+export async function GET(req: NextRequest) {
+  try {
+    const url = new URL(req.nextUrl);
+    const productId = url.searchParams.get("productId");
+    var product: Object = {};
+    var responseCode = 500;
+    if (productId) {
+      const productRef = doc(db, "products", productId);
+
+      await getDoc(productRef)
+        .then((doc) => {
+          if (doc.exists()) {
+            product = doc.data() as Product;
+            responseCode = 200;
+          } else {
+            throw new CustomError("Product not found.", 404);
+          }
+        })
+        .catch((error) => {
+          throw new CustomError(error.message, error.code);
+        });
+    } else {
+      throw new CustomError("Invalid request: ProductId missing.", 400);
+    }
+    return NextResponse.json(
+      {
+        data: product,
+      },
+      { status: responseCode }
+    );
+  } catch (error: any) {
+    return handleApiError(error);
+  }
+}
+
+export const revalidate =
+  60 * parseInt(process.env.NEXT_PUBLIC_FIRESTORE_REVALIDATE || "60");
+// This is the revalidate time in minutes. Fallsback to 60 minutes if not provided in .env
