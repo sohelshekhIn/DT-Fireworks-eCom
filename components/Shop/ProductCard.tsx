641f04974b144c715f7866a64d4b984351fd3ee1
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-10 21:10:16 -0400

    refactor: Update ProductCard component to use AddToCartBtn from CartComps

--- components/Shop/ProductCard.tsx
+++ components/Shop/ProductCard.tsx
@@ -2,20 +2,20 @@ import type { Product } from "@/types/product";
 import Image from "next/image";
 import Link from "next/link";
 import { DisplayPrice } from "./ProductPageComps";
+import { AddToCartBtn } from "./CartComps";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Link
-      href={`/shop/product/${product.id}`}
-      className="flex flex-col bg-white hover:shadow-md max-w-80 min-w-60 border shadow-sm rounded-md dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70"
-    >
-      <Image
-        width={400}
-        height={400}
-        className="w-full h-72 rounded-t-md object-cover"
-        src={product.media.images[0]}
-        alt={`Image of ${product.name}`}
-      />
+    <div className="flex flex-col bg-white hover:shadow-md max-w-80 min-w-60 border shadow-sm rounded-md dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+      <Link href={`/shop/product/${product.id}`}>
+        <Image
+          width={400}
+          height={400}
+          className="w-full h-72 rounded-t-md object-cover"
+          src={product.media.images[0]}
+          alt={`Image of ${product.name}`}
+        />
+      </Link>
       <div className="p-4 md:p-5 mt-5">
         <h3 className="text-lg font-bold text-gray-800 dark:text-white">
           {product.name}
@@ -27,12 +27,10 @@ const ProductCard = ({ product }: { product: Product }) => {
         </p>
         <div className="flex items-center justify-between mt-4">
           <DisplayPrice product={product} size="small" />
-          <button className="px-3 py-2 text-sm font-semibold text-white bg-primary hover:bg-primaryDark rounded-md dark:bg-primaryDark">
-            Add to Cart
-          </button>
+          <AddToCartBtn product={product} />
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
