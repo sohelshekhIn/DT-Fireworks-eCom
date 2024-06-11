2bac9a4cdae1bc0d02dddfe05a8e754ed813b28f
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-11 04:27:08 -0400

    refactor: Update revalidate-cache route to handle tag parameter in GET request

--- app/api/revalidate-cache/route.ts
+++ app/api/revalidate-cache/route.ts
@@ -1,7 +1,20 @@
 import { revalidateTag } from "next/cache";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  revalidateTag("categories");
-  return NextResponse.json({ data: {} }, { status: 200 });
+export async function GET(req: NextRequest) {
+  const url = new URL(req.nextUrl);
+  const tag = url.searchParams.get("tag");
+  if (tag) {
+    revalidateTag(tag);
+    return NextResponse.json(
+      {
+        data: {
+          message: "Cache revalidation request sent",
+        },
+      },
+      { status: 200 },
+    );
+  } else {
+    return NextResponse.json({ error: "Tag not found" }, { status: 400 });
+  }
 }
