d21b551e6e339328ef2c94146fa73ec9912e7f2b
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-15 21:47:29 -0400

    refactor: Update SignInPage layout and styles

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <div className="max-w-[85vw] md:w-1/2 lg:w-5/12 mx-auto py-10 md:pt-28 pb-36">
-      <div className="min-w-80 max-w-3xl p-4 mx-auto bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700">
+    <div className="mx-auto max-w-[85vw] py-10 pb-36 md:w-1/2 md:pt-28 lg:w-5/12">
+      <div className="mx-auto min-w-80 max-w-3xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm dark:border-neutral-700 dark:bg-neutral-900">
         <div className="p-4 sm:p-7">
           <div className="text-center">
             <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
@@ -14,7 +14,7 @@ const SignInPage = () => {
             <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
               Don&apos;t have an account yet?{" "}
               <Link
-                className="text-primary decoration-2 hover:underline font-medium dark:text-primary"
+                className="font-medium text-primary decoration-2 hover:underline dark:text-primary"
                 href="/signup"
               >
                 Sign up here
@@ -25,7 +25,7 @@ const SignInPage = () => {
           <div className="mt-5">
             <SignInWithGoogleButton />
 
-            <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
+            <div className="flex items-center py-3 text-xs uppercase text-gray-400 before:me-6 before:flex-1 before:border-t before:border-gray-200 after:ms-6 after:flex-1 after:border-t after:border-gray-200 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
               Or
             </div>
 
