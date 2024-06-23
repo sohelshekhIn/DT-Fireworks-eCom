fd518fc0572b471d633fc7900f0caf2057b09182
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-22 20:16:23 -0400

    refactor: Update cart data handling and syncing

---
+++ utils/syncCartData.ts
@@ -0,0 +1,23 @@
+"use client";
+
+export const saveData = async (data: any) => {
+  try {
+    fetch("/api/cart/sync", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data }),
+    });
+  } catch (error) {
+    console.error("Error while saving data", error);
+  }
+};
+
+export const loadData = async () => {
+  const response = await fetch("/api/cart/sync");
+  if (!response.ok) {
+    throw new Error("Error while fetching data");
+  }
+  return response.json();
+};
