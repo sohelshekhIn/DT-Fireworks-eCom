050a14e309d7a7bef42bc2186f909b74681030f4
Sohel Shekh <contact.sohelsehekh@gmail.com>
2024-06-24 01:42:56 -0400

    refactor: Add user session tags and cache settings to checkUserAuthStatus

--- utils/checkUserAuthStatus.ts
+++ utils/checkUserAuthStatus.ts
@@ -1,13 +1,14 @@
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import appUrl from "./apiCallHandler";
-import { cache } from "react";
 
 export const checkUserAuthStatus = async (
   session: RequestCookie | undefined,
 ) => {
-  console.log(session);
-
   const res = await fetch(appUrl("/api/login"), {
+    next: {
+      tags: ["user-session"],
+      revalidate: 60 * 30, // 30 minutes
+    },
     cache: "force-cache",
     headers: {
       Cookie: `session=${session?.value}`,
