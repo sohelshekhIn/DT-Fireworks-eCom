94c90912f47578c1f6bebfa590d2dfd72a7627a1
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-07-02 17:08:10 -0400

    refactor: Add AuthorizingPayment component for payment authorization

---
+++ components/Order/AuthorizingPayment.tsx
@@ -0,0 +1,16 @@
+import { SandClock } from "@/public/images";
+import Image from "next/image";
+
+export const AuthorizingPayment = () => {
+  return (
+    <div className="absolute inset-0 z-10 flex h-[100vh] w-full flex-col items-center justify-center gap-5 space-x-2 bg-white">
+      <Image
+        src={SandClock}
+        alt="Sand Clock Icon to describe wait till we verufy the transaction"
+      />
+      <p className="font-lg font-semibold">
+        Please wait while we authorize your payment...
+      </p>
+    </div>
+  );
+};
