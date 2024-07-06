be68da985cd8747f024ecefad3354851e4f392ec
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-07-06 14:08:19 -0400

    refactor: Add authorization check for retrieving order details by ID

--- app/api/order/one/route.ts
+++ app/api/order/one/route.ts
@@ -2,6 +2,7 @@ import { customInitApp } from "@/lib/firebase-admin-config";
 import { db } from "@/lib/firebase-config";
 import { Order } from "@/types/order";
 import { CustomError, handleApiError } from "@/utils/apiErrorHandler";
+import { auth } from "firebase-admin";
 import { doc, getDoc } from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -17,16 +18,29 @@ export const GET = async (req: NextRequest) => {
       throw new CustomError("Order id is required", 400);
     }
 
+    const session = req.cookies.get("session");
+    if (!session) {
+      throw new CustomError("Unauthorized", 401);
+    }
+
+    const user = await auth().verifySessionCookie(session.value, true);
+    const uid = user.uid;
+
     // get order by id
     const orderRef = doc(db, "online-orders", orderId);
     var orderData: Partial<Order> | null = null;
     await getDoc(orderRef)
       .then((doc) => {
         if (doc.exists()) {
-          orderData = {
-            id: doc.id,
-            ...doc.data(),
-          };
+          const docData: Partial<Order> = doc.data();
+          if (docData.uid !== uid) {
+            throw new CustomError("Unauthorized", 401);
+          } else {
+            orderData = {
+              id: doc.id,
+              ...docData,
+            };
+          }
         } else {
           throw new CustomError("Order not found", 404);
         }
