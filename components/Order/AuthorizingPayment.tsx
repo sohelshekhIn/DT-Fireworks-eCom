618a518bf3a7147764ebbe289ad60eb02ecafb5e
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-07-06 13:21:54 -0400

    refactor: Update AuthorizingPayment component to use full height instead of fixed viewport height

--- components/Order/AuthorizingPayment.tsx
+++ components/Order/AuthorizingPayment.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const AuthorizingPayment = () => {
   return (
-    <div className="absolute inset-0 z-10 flex h-[100vh] w-full flex-col items-center justify-center gap-5 space-x-2 bg-white">
+    <div className="absolute inset-0 z-10 flex h-full w-full flex-col items-center justify-center gap-5 space-x-2 bg-white">
       <Image
         src={SandClock}
         alt="Sand Clock Icon to describe wait till we verufy the transaction"
