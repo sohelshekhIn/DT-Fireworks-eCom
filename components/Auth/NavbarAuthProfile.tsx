25922b0dfcaf917e3149a4b98c8b2aac54e07e21
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-07-06 13:21:46 -0400

    refactor: Update NavbarAuthProfile component to include full width for My Orders link

--- components/Auth/NavbarAuthProfile.tsx
+++ components/Auth/NavbarAuthProfile.tsx
@@ -54,7 +54,7 @@ export const NavbarAuthProfile = () => {
               My Account
             </Link> */}
             <Link
-              className="flex items-center gap-x-3.5 rounded-lg px-3 py-3 text-sm text-gray-800 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
+              className="flex w-full items-center gap-x-3.5 rounded-lg px-3 py-3 text-sm text-gray-800 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700"
               href="/account/my-orders"
             >
               My Orders
