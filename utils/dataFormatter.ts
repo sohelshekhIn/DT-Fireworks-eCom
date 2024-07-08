14c6f7e6bdb845d0f51d2cfa9786d56d5b140bec
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-07-08 14:49:48 -0400

    refactor: Update formatUserFriendlyDate to correctly display the date of the month

--- utils/dataFormatter.ts
+++ utils/dataFormatter.ts
@@ -1,7 +1,7 @@
 export const formatUserFriendlyDate = (date: string) => {
   let tempDate = new Date(date);
   const month = tempDate.toLocaleString("default", { month: "long" });
-  return `${tempDate.getDay()} ${month} ${tempDate.getFullYear()}`;
+  return `${tempDate.getDate()} ${month} ${tempDate.getFullYear()}`;
 };
 
 export const formatUserFriendlyTime = (date: string) => {
