13fb01aa25305713b16b75a0c5e788dfc1eab5e1
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-08-16 12:44:48 -0400

    refactor: Update firebase-admin-config.ts to use environment variables for Firebase Admin credentials

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,8 +1,14 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { credential } from "firebase-admin";
+import { initializeApp, getApps } from "firebase-admin/app";
 const serviceAccount = require("@/service-account.json");
 
 const firebaseAdminConfig = {
-  credential: cert(serviceAccount),
+  credential: credential.cert({
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    
+  })
 };
 
 export function customInitApp() {
