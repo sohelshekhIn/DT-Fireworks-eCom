f153bd395e78d5ad3bfe0ba67354dc7466d9dd82
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-08-16 12:47:14 -0400

    removed unused import for the firebase admin

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,14 +1,12 @@
 import { credential } from "firebase-admin";
 import { initializeApp, getApps } from "firebase-admin/app";
-const serviceAccount = require("@/service-account.json");
 
 const firebaseAdminConfig = {
   credential: credential.cert({
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-    
-  })
+  }),
 };
 
 export function customInitApp() {
