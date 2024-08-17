a1ba57a59781bc89f1ffcd900e838735db690c4f
Sohel Shekh <contact.sohelshekh@gmail.com>
2024-08-17 15:55:19 -0400

    refactor: Update firebase-admin-config.ts to handle parsing of FIREBASE_PRIVATE_KEY environment variable

--- lib/firebase-admin-config.ts
+++ lib/firebase-admin-config.ts
@@ -1,10 +1,12 @@
 import { credential } from "firebase-admin";
 import { initializeApp, getApps } from "firebase-admin/app";
 
+const { privateKey } = JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY || "");
+
 const firebaseAdminConfig = {
   credential: credential.cert({
+    privateKey,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
   }),
 };
