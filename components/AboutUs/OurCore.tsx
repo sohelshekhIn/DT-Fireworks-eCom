9c0e39671b326ac27b9e6d23cfb94b8e0c90f2dc
Sohel Shekh <hi@sohel.tech>
2025-03-14 07:41:15 -0400

    refactor: Update team member details and improve layout in OurCore component

--- components/AboutUs/OurCore.tsx
+++ components/AboutUs/OurCore.tsx
@@ -2,28 +2,28 @@ import Image from "next/image";
 
 const OurCore = () => {
   return (
-    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <div className="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
       {" "}
-      <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
-        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+      <div className="mx-auto mb-10 max-w-2xl text-center lg:mb-14">
+        <h2 className="text-2xl font-bold dark:text-white md:text-4xl md:leading-tight">
           Our Core Team
         </h2>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-8 md:gap-12">
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-3 md:gap-12">
         <div className="text-center">
           <Image
             width={200}
             height={200}
-            className="rounded-full sm:size-48 lg:size-60 mx-auto"
-            src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
+            className="mx-auto rounded-full sm:size-48 lg:size-60"
+            src="https://ui-avatars.com/api/?name=Chandrakath+Chhatriwala"
             alt="Image Description"
           />
           <div className="mt-2 sm:mt-4">
-            <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-              Maria Powers
+            <h3 className="text-sm font-medium text-gray-800 dark:text-neutral-200 sm:text-base lg:text-lg">
+              Chandrakath Chhatriwala
             </h3>
-            <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-              Director of sales
+            <p className="text-xs text-gray-600 dark:text-neutral-400 sm:text-sm lg:text-base">
+              Partners
             </p>
           </div>
         </div>
@@ -32,16 +32,16 @@ const OurCore = () => {
           <Image
             width={200}
             height={200}
-            className="rounded-full sm:size-48 lg:size-60 mx-auto"
-            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
+            className="mx-auto rounded-full sm:size-48 lg:size-60"
+            src="https://ui-avatars.com/api/?name=Chirag+Chhatriwala"
             alt="Image Description"
           />
           <div className="mt-2 sm:mt-4">
-            <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-              Delia Pawelke
+            <h3 className="text-sm font-medium text-gray-800 dark:text-neutral-200 sm:text-base lg:text-lg">
+              Chirag Chhatriwala
             </h3>
-            <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-              Front-end Developer
+            <p className="text-xs text-gray-600 dark:text-neutral-400 sm:text-sm lg:text-base">
+              Partners
             </p>
           </div>
         </div>
@@ -50,16 +50,16 @@ const OurCore = () => {
           <Image
             width={200}
             height={200}
-            className="rounded-full sm:size-48 lg:size-60 mx-auto"
-            src="https://images.unsplash.com/photo-1624224971170-2f84fed5eb5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
+            className="mx-auto rounded-full sm:size-48 lg:size-60"
+            src="https://ui-avatars.com/api/?name=Nikunj+Chhatriwala"
             alt="Image Description"
           />
           <div className="mt-2 sm:mt-4">
-            <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg dark:text-neutral-200">
-              Tom Lowry
+            <h3 className="text-sm font-medium text-gray-800 dark:text-neutral-200 sm:text-base lg:text-lg">
+              Nikunj Chhatriwala
             </h3>
-            <p className="text-xs text-gray-600 sm:text-sm lg:text-base dark:text-neutral-400">
-              UI/UX Designer
+            <p className="text-xs text-gray-600 dark:text-neutral-400 sm:text-sm lg:text-base">
+              Partners
             </p>
           </div>
         </div>
